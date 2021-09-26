02e168c1d8c8f93530bee1113b4d9b565e5d87fc
lmuffato <lmiremuffato@gmail.com>
2021-09-26 11:50:56 -0300

    requisito 1

--- middlewares/users/createUser.js
+++ middlewares/users/createUser.js
@@ -3,7 +3,7 @@ const usersServices = require('../../services/usersServices');
 const createUser = async (req, res) => {
     const { name, email, password } = req.body;
     const data = await usersServices.create(name, email, password);
-    return res.status(201).json(data);
+    return res.status(201).json({ user: data });
 };
 
 module.exports = { createUser };
--- models/connection.js
+++ models/connection.js
@@ -6,7 +6,7 @@ const OPTIONS = {
 };
 
 const connection = async () => {
-  const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster'; // Conexão para o avaliador.
+  const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster'; // Conexão para o avaliador
   // const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster'; // Conexão normal
   const DB_NAME = 'Cookmaster';
   try {
