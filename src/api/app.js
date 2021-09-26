a720ce3b283929fb182d0202db4a13191e97bbc1
lmuffato <lmiremuffato@gmail.com>
2021-09-26 18:05:55 -0300

    requisito 2

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const usersControllers = require('../../controllers/usersControllers');
+const loginControllers = require('../../controllers/loginControllers');
 
 const app = express();
 
@@ -14,5 +15,8 @@ app.use(bodyParser.json());
 
 // Todas as rotas a partir de /user serão tratadas no usersControllers;
 app.use('/users', usersControllers);
+
+// Todas as rotas a partir de /login serão tratadas no loginControllers;
+app.use('/login', loginControllers);
 
 module.exports = app;
