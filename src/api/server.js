a28cf93a77217e0dfaaaa23cc85ebe9f472701ad
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-09-28 12:37:13 -0300

    Server working message fixed

--- src/api/server.js
+++ src/api/server.js
@@ -2,4 +2,4 @@ const app = require('./app');
 
 const PORT = 3000;
 
-app.listen(PORT, () => console.log(`Server Working on ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server Working on Port ${PORT} 🚀`));
